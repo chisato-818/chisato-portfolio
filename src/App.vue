114999a18adbec0b48c729c751124a067500cb1b
chisato-818 <chisa818.milkyyy@gmail.com>
2022-05-28 16:30:52 +0900

    プロフィール完成

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,15 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 nav {
@@ -21,7 +20,6 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="body">
+    <div class="head">
+      <h1>わたしについて</h1>
+    </div>
+    <div class="profile">
+      <div class="leftPro">
+        <ul>
+          <li>Name : chisato takahashi</li>
+          <li>Age : 22</li>
+          <li>Born : 1999.8.18</li>
+          <li>Blood type : A</li>
+          <li>Height : 175.6cm</li>
+          <li>Hometown : chiba(kamagaya)</li>
+        </ul>
+      </div>
+      <div class="rightPro">
+        <img src="@/assets/chisamikan.jpg" alt="" class="photo" />
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.head {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 6px solid #000;
+  height: 100px;
+  margin: 0px 100px;
+  padding: 40px;
+}
+.head h1 {
+  font-size: 40px;
+  letter-spacing: 30px;
+}
+.profile {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 120px;
+}
+.leftPro {
+  margin-right: 170px;
+}
+
+.leftPro ul {
+  list-style: none;
+  letter-spacing: 5px;
+  line-height: 55px;
+}
+.photo {
+  height: 500px;
+}
+</style>
